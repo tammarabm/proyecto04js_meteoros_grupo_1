ae7109cb4dcd558b55f247434edb74af9e86bdb2
tammarabm <165035795+tammarabm@users.noreply.github.com>
2024-09-26 11:14:06 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const app=express();
+
+//endpoint metodos get o post
+
+app.get('/', (req, res)=>{
+    /*res.end("Bienvenido a mi servidor backend");
+    console.log(__dirname);*/
+    res.sendFile(__dirname+"public/views/index.html");
+});
+
+//routing
+app.use('/src', express.static(__dirname+"/src")); 
+app.use('/public', express.static(__dirname+"/public"));
+app.use('/node_modules', express.static(__dirname+"/node_modules"));
+
+//listening
+app.listen(5001, ()=>{
+    console.log("Servidor Node corriendo perfectamente");
+});
