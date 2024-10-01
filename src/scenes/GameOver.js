77045a5cfce23707309bce2dd2fa5b0ad9a3ac5b
LarraSanti <santiagolarraburu4@gmail.com>
2024-09-30 21:01:26 -0300

    Game_over_imagen

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -7,9 +7,20 @@ class GameOver extends Phaser.Scene {
         this.puntaje = data.puntaje; //Recibir el puntaje
     }
 
+    init(data){
+        this.puntajeMaximo = data.puntajeMaximo; //Recibe el puntaje maximo
+    }
+
+    preload(){
+        this.load.image('backgroundGameOver', '/public/resources/img/backgroundGameOver.png');
+    }
+    
+
     create() {
+        this.background = this.add.tileSprite(663, 298, 1326, 596, 'backgroundGameOver');
         this.add.text(663, 200, 'Game Over', { fontSize: '64px', fill: '#fff' }).setOrigin(0.5);
         this.add.text(663, 300, 'Puntaje: ' + this.puntaje, { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
+        this.add.text(663,500, 'Puntaje máximo: '+ this.puntajeMaximo, {fontSize: '32px',fill: '#fff'}).setOrigin(0.5);
         this.add.text(663, 400, 'Barra espaciadora para volver a jugar', { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
 
         this.input.keyboard.once('keydown-SPACE', () => {
